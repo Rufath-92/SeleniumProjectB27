03df84410f0917c8256f57e175b8f0f59813aeed
Rufat <huseynovrufat.usa@gmail.com>
2022-09-15 13:58:58 -0400

    ..

---
+++ src/test/java/com/cydeo/tests/day1_selenium_intro/PO03_GettingPageTitle.java
@@ -0,0 +1,35 @@
+package com.cydeo.tests.day1_selenium_intro;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class PO03_GettingPageTitle {
+    public static void main(String[] args) {
+
+        //set up chrome and create WebDriver instance
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+
+
+        // navigate to google and print out the title
+        driver.get("https://cydeo.com");
+
+        String title = driver.getTitle();
+        System.out.println("Title of the page: " + title);
+
+        if(title.equals("Cydeo")){
+            System.out.println("Test passed");
+        } else{
+            System.out.println("Test failed");
+        }
+
+
+
+
+
+
+        driver.quit();
+    }
+}
